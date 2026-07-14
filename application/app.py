749d2964bb5532a4bd431158e6544c0280f51a5e
harish-112 <harishmunusami.work@gmail.com>
2026-07-14 15:29:02 +0530

    chore(docker): update docker base image
    docs(api): update the comment line

--- application/app.py
+++ application/app.py
@@ -1,3 +1,5 @@
+#i created a simple FastAPI application to containerize and run it inside azure container app
+
 from fastapi import FastAPI
 import uvicorn
 import random
@@ -6,7 +8,6 @@ app = FastAPI()
 
 @app.get("/site-safety")
 def check_safety():
-    # Simulating a live weather tracking check for a construction site
     conditions = ["Clear Skies - Safe to Work", "Heavy Rain - Stop Outdoor Work", "High Winds - Stand Down Cranes"]
     current_status = random.choice(conditions)
     return {
