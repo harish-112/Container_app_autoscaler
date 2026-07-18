e51e60e83c295d93c1c443ce0215a55c5278c9b0
harish-112 <harishmunusami.work@gmail.com>
2026-07-18 10:50:47 +0530

    feat(azure): implement scaling of replicas in container apps

---
+++ replica.py
@@ -0,0 +1,30 @@
+import json
+import subprocess
+
+from config import (
+    RESOURCE_GROUP,
+    CONTAINER_APP_NAME
+)
+
+def get_current_replicas():
+
+    command = [
+        r"C:\Program Files\Microsoft SDKs\Azure\CLI2\wbin\az.cmd",
+        "containerapp",
+        "show",
+        "--resource-group",RESOURCE_GROUP,
+        "--name",CONTAINER_APP_NAME,
+        "--query","properties.template.scale.minReplicas",
+        "--output","json"
+    ]
+
+    result = subprocess.run(
+        command,
+        capture_output=True,
+        text=True
+    )
+
+    if result.returncode != 0:
+        raise RuntimeError(result.stderr)
+
+    return int(json.loads(result.stdout))
